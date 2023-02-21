d580a1c669cd4332ce775a59fadb75778db29a67
mak2002 <makbond0902@gmail.com>
2023-02-21 18:14:33 +0530

    separate route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, StrictMode } from "react";
 
-function App() {
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  ReactRouter,
+  Route,
+  RootRoute,
+} from "@tanstack/react-router";
+import { Projects } from "./Pages/Projects";
+import About from "./Pages/About";
+import { Home } from "./Pages/Home";
+
+// Create a root route
+const rootRoute = new RootRoute({
+  component: Root,
+});
+
+function Root() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex h-screen jc">
+      <div className="flex flex-col gap-24 items-center justify-center h-screen w-1/4 bg-slate-600">
+        <Link to="/" className="bg-gray-400 p-4">
+          Home
+        </Link>
+        <Link to="/about" className="bg-gray-400 p-4">
+          About
+        </Link>
+        <Link
+          to="/projects/$projectId"
+          className="bg-gray-400 p-4"
+          params={{
+            projectId: "my-first-blog-post",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Projects
+        </Link>
+
+        <Link
+          to="/projects/$projectId"
+          search={{
+            pageIndex: 10,
+            includeCategories: ["JS", "TS"],
+            sortBy: "alpha",
+            desc: true,
+          }}
+          className="bg-gray-400 p-4"
+          params={{
+            projectId: "SearchParams",
+          }}
+        >
+          Search Params
+        </Link>
+      </div>
+      <hr />
+      <div className="flex justify-center items-center w-full">
+        <Outlet />
+      </div>
     </div>
   );
 }
 
-export default App;
+// Create an index route
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
+});
+
+const aboutRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: About,
+});
+
+const projectsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/projects",
+});
+
+// Create a route with a dynamic parameter
+const projectRoute = new Route({
+  getParentRoute: () => projectsRoute,
+  path: "$projectId",
+  component: Projects,
+});
+
+// Router Tree
+const routeTree = rootRoute.addChildren([
+  homeRoute,
+  aboutRoute,
+  projectsRoute.addChildren([projectRoute]),
+]);
+
+const router = new ReactRouter({ routeTree });
+
+// Register your router for maximum type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export default function App() {
+  return (
+    <div>
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>
+    </div>
+  );
+}
---
+++ src/Pages/Projects.tsx
@@ -0,0 +1,17 @@
+import { useParams } from "@tanstack/react-router";
+import React from "react";
+
+export function Projects() {
+  const { projectId } = useParams();
+
+  
+
+  return (
+    <div>
+      <p className="text-3xl font-bold">
+        Project name:  {"  "}
+        <span className="underline">{projectId}</span>
+      </p>
+    </div>
+  );
+}
